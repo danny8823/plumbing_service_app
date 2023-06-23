8bebf10f7b52c4002b71f5f6b8f8e56f631085a3
danny8823 <dannyyoo714@gmail.com>
2023-06-22 22:14:26 -0700

    added icons/animation, added home page styling

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import {BsStar} from 'react-icons/bs'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome home...</h1>
+        <div className = "m-auto">
+            <h1 className="text-4xl font-bold m-auto text-center">Pro's Plumbing Inc</h1>
+            <img src = "https://images.unsplash.com/photo-1615529328331-f8917597711f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt = 'water with bubbles'/>
+            <p className = "text-3xl text-center mt-10">Get our 5 star service with just a few clicks</p>
+            <div className="flex justify-center m-auto">
+                <h1 className="star-icon"><BsStar/></h1>
+                <h1 className="star-icon"><BsStar/></h1>
+                <h1 className="star-icon"><BsStar/></h1>
+                <h1 className="star-icon"><BsStar/></h1>
+                <h1 className="star-icon"><BsStar/></h1>
+            </div>
+            <img src = 'https://images.unsplash.com/photo-1594761051656-a37da88b5f5b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
+            alt = "plumber working"/>
         </div>
     )
 }
